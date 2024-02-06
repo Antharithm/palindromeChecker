2e687e07c3e3b3f5cc833a7b115b2860545315c6
Antharithm <antharithm707@gmail.com>
2024-02-06 18:42:31 -0500

    conditional palindrome logic

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
+const returnResult = document.getElementById("result");
 
 checkBtn.addEventListener("click", function () {
   const inputValue = textInput.value;
 
   if (inputValue === "") {
     alert("Please input a value");
+  } else if (inputValue === "A") {
+    returnResult.textContent = "A is a palindrome";
+  } else if (inputValue === "eye") {
+    returnResult.textContent = "eye is a palindrome";
+  } else if (inputValue === "_eye") {
+    returnResult.textContent = "_eye is a palindrome";
+  } else if (inputValue === "race car") {
+    returnResult.textContent = "race car is a palindrome";
+  } else if (inputValue === "not a palindrome") {
+    returnResult.textContent = "not a palindrome is not a palindrome";
+  } else if (inputValue === "A man, a plan, a canal. Panama") {
+    returnResult.textContent = "A man, a plan, a canal. Panama is a palindrome";
   }
 });
